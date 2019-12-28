322f9fed7b616a9b672dd067786e494dbf873aab
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-12-28 05:42:48 -0500

    authenticating users with signed cookies

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
 const next = require("next");
 const express = require("express");
+const axios = require("axios");
+const cookieParser = require("cookie-parser");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 4000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const AUTH_USER_TYPE = "authenticated"
+// const COOKIE_SECRET = process.env.COOKIE_SECRET;
+const COOKIE_SECRET = "ddsdfdsfdlsllflfl";
+const COOKIE_OPTIONS = {
+    httpOnly: true,
+    secure: !dev,
+    signed: true
+}
+
+const authenticate = async (email, password) => {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+    return data.find(user => {
+        if(user.email === email && user.website === password){
+            return user;
+        }
+    })
+}
+
 app.prepare().then(() => {
     const server = express();
     server.use(express.json());
+    server.use(cookieParser(COOKIE_SECRET));
 
-    server.post('/api/login', (req, res) => {
-        const { email, password } = req.body;
-        res.json({
-            email,
-            password,
-            success: true
-        });
+    server.post('/api/login', async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const user = await authenticate(email, password);
+
+            // if(!user){
+            //     return res.status(403).send("invalid email or password");
+            // }
+
+            const userData = {
+                name: user.name,
+                email: user.email,
+                type: AUTH_USER_TYPE
+            }
+            res.cookie('token', userData, COOKIE_OPTIONS);
+            res.json(userData);
+
+        } catch (error) {
+            return res.status(403).send("invalid email or password");
+        }
     });
 
     server.get("*", (req, res) => {
